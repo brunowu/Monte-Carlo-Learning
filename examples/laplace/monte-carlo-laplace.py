d5c0e6c466b48d35411d1bbec4cb44a948755e10
brunowu <xinzhe.wu1990@gmail.com>
2017-08-09 14:12:56 +0200

    laplace fine

--- examples/laplace/monte-carlo-laplace.py
+++ examples/laplace/monte-carlo-laplace.py
@@ -1,28 +1,31 @@
 import random as rd
 import math as mt
 
-NUM_SAMPLING = 1000
-STEP  = 0.10
-
+NUM_SAMPLING = 100000
+STEP  = 0.1
+K = 50
 def main():
-	SUM  = 0.0
-	x = 2.0
-	y = 0.0
+	SUM = 0.0
+	count = 0
 	for _ in range(NUM_SAMPLING):
-             	a = 2*rd.randint(0,1) - 1
-		b = 2*rd.randint(0,1) - 1
-		x = x + STEP*a
-		y = y + STEP*b
-		r = mt.sqrt(x**2+y**2)
-		if r <= 1.0:
-			SUM = SUM + 4
-		elif r >= 3.0:
-			SUM = SUM + 6
-
-	val = SUM/NUM_SAMPLING
-
+		x = 2.0
+		y = 0.0
+		for _ in range(K):
+			a = 2*rd.randint(0,1) - 1
+			b = 2*rd.randint(0,1) - 1
+			x = x + STEP*a
+			y = y + STEP*b
+			r = mt.sqrt(x**2+y**2)
+			if r <= 1.0:
+				count = count + 1
+				SUM = SUM + 4
+				break
+			if r >= 3.0:
+				count = count + 1
+				SUM = SUM + 6
+				break  
 	print SUM					
-	print val
+	print SUM/count
 
 if __name__ == "__main__":
 	main()
